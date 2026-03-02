e944ba175c69daca5192f90739e605dc07e0ce42
mnahtygal <52798812+mnahtygal@users.noreply.github.com>
2026-03-02 10:36:09 -0500

    Add initial project folder structure

---
+++ push.bat
@@ -0,0 +1,12 @@
+echo.> docs\.gitkeep
+echo.> printers\.gitkeep
+echo.> profiles\.gitkeep
+echo.> calibration\.gitkeep
+echo.> models\.gitkeep
+echo.> gcode\.gitkeep
+echo.> scripts\.gitkeep
+echo.> assets\.gitkeep
+
+git add .
+git commit -m "Add initial project folder structure"
+git push
